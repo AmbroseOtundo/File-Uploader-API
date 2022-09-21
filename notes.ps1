2a19b365fea1486fa67447576bfd562cdc7a43c8
ambroseotundo <ambroseotundo9@gmail.com>
2022-09-21 13:57:10 -0400

    creating the upload images API

--- notes.ps1
+++ notes.ps1
@@ -19,3 +19,6 @@
     class ProductImage(models.Model):
         product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
         image = models.ImageField(upload_to='store/images')
+
+# Building an API to upload images
+ -- an endpoint -- : /products/1/images/
