427cc42a56c3125e08a108fc3b8a38ea2eec6c21
ambroseotundo <ambroseotundo9@gmail.com>
2022-09-30 11:23:41 +0100

    debugging the model error. unable to resolve it

--- store/models.py
+++ store/models.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
-from uuid import uuid4
-import uuid
+# from uuid import uuid4
+# import uuid
 
 from store.validators import validate_file_size
 
